904470500f063c4a4c635db378556cccb46d8b0d
Luis Rodriguez <luisrs8608@gmail.com>
2022-12-06 20:19:06 -0300

    second commit

--- src/ServiciosCombos.java
+++ src/ServiciosCombos.java
@@ -11,4 +11,31 @@ public class ServiciosCombos extends Servicios{
         this.servicios = servicios;
         this.descuento = descuento;
     }
+
+    public ArrayList<Servicios> getServicios() {
+        return servicios;
+    }
+
+    public void setServicios(ArrayList<Servicios> servicios) {
+        this.servicios = servicios;
+    }
+
+    public double getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(double descuento) {
+        this.descuento = descuento;
+    }
+
+    @Override
+    public double calcularPrecio() {
+        double precio = 0.0;
+
+        for (Servicios servicio : this.servicios) {
+            precio += servicio.calcularPrecio();
+        }
+
+        return precio - (precio / 100 * this.descuento);
+    }
 }
